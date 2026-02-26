588ee5f29ec81a30141097e15daec62eb6c9fa26
Mehrab <programmer.py.mail@gmail.com>
2026-02-26 13:53:16 +0330

    install redis and celery and creating a task with celery(shared_task decorator)

--- core/account/urls.py
+++ core/account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path , include
 
+from . import views
+
 app_name = "api_v1"
 
 urlpatterns = [
-    path("api/v1/",include("account.api.v1.urls"))
+    path("api/v1/",include("account.api.v1.urls")),
+    path("send-email/",views.send_email,name="send_email")
 
 ]
--- core/core/settings.py
+++ core/core/settings.py
@@ -162,3 +162,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://127.0.0.1:8000"
 ]
+
+# celery config
+CELERY_BROKER_URL = "redis://redis:6379/1 "
